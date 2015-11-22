03554735d923f21312155df51f85339475196ef5
VDMT <viranthat@gmail.com>
2015-11-22 14:04:22 +1300

    Initial commit of design patterns sample - (Creational) Abstract Factory, Builder

---
+++ Creational_AbstractFactor/FooBar.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creational_AbstractFactory
+{
+    class FooBar
+    {
+    }
+
+    public interface IBody
+    {
+        string BodyParts { get; }
+    }
+
+    public class CarBody : IBody
+    {
+        public virtual string BodyParts
+        {
+            get { return "Body shell parts for a car"; }
+        }
+    }
+
+    public class VanBody: IBody
+    {
+        public virtual string BodyParts
+        {
+            get { return "Body shell parts for a van"; }
+        }
+    }
+
+    public interface IChassis
+    {
+        string ChassisParts { get; }
+    }
+
+    public class CarChassis : IChassis
+    {
+        public virtual string ChassisParts
+        {
+            get { return "Chassis parts for a Car"; }
+        }
+    }
+
+    public class VanChassis : IChassis
+    {
+        public virtual string ChassisParts
+        {
+            get { return "Chassis parts for a Van"; }
+        }
+    }
+
+    public interface IGlassware
+    {
+        string GlasswareParts { get; }
+    }
+
+    public class CarGlassware : IGlassware
+    {
+        public virtual string GlasswareParts
+        {
+            get { return "Window glassware for a car"; }
+        }
+    }
+
+    public class VanGlassware : IGlassware
+    {
+        public virtual string GlasswareParts
+        {
+            get { return "Window glassware for a van"; }
+        }
+    }
+
+    ///// pattern involves a abstract factory class and two concrete factory classes 
+    public abstract class AbstractVehicleFactory
+    {
+        public abstract IBody CreateBody();
+        public abstract IChassis CreateChassis();
+        public abstract IGlassware CreateGlassware();
+    }
+
+    public class CarFactory : AbstractVehicleFactory
+    {
+        public override IBody CreateBody()
+        {
+            return new CarBody();
+        }
+
+        public override IChassis CreateChassis()
+        {
+            return new CarChassis();
+        }
+
+        public override IGlassware CreateGlassware()
+        {
+            return new CarGlassware();
+        }
+    }
+
+    public class VanFactory : AbstractVehicleFactory
+    {
+        public override IBody CreateBody()
+        {
+            return new VanBody();
+        }
+
+        public override IChassis CreateChassis()
+        {
+            return new VanChassis();
+        }
+
+        public override IGlassware CreateGlassware()
+        {
+            return new VanGlassware();
+        }
+    }
+}
---
+++ Creational_AbstractFactor/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creational_AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // the client can instantiate the appropriate
+            // 'factory' with which it can obtain the correct
+            // parts, whether for car or van
+
+            string whatToMake = "car";
+
+            AbstractVehicleFactory factory = null;
+
+            // create the correct factory
+            if (whatToMake.Equals("car"))
+            {
+                factory = new CarFactory();
+            }
+            else if (whatToMake.Equals("van"))
+            {
+                factory = new VanFactory();
+            }
+
+            // create the vehicle component parts...
+            IBody body = factory.CreateBody();
+            IChassis chassis = factory.CreateChassis();
+            IGlassware glass = factory.CreateGlassware();
+
+            // see what we have created
+            Console.WriteLine(body.BodyParts);
+            Console.WriteLine(chassis.ChassisParts);
+            Console.WriteLine(glass.GlasswareParts);
+            Console.ReadLine();
+
+            // disadvantage: if adding a new product ex. Lights, to include lights in family of components
+            //              would need to amend AbstractVehicleFactory, CarFactory, VanFactory
+            //              in addition new ILights (CarLights/Vanlights) will need to be created
+
+        }
+    }
+
+
+}
